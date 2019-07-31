3ca32474711e57ffc63d8fc1142a999f16d73851
Justin <jelias153@g.rwu.edu>
2019-07-30 21:26:32 -0500

    Committing the rest of the files for previous commit

--- DiceApp/Dicee/Dicee/ViewController.swift
+++ DiceApp/Dicee/Dicee/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //var NameOfVariable: type of variable = value
+    var RandomDiceIndex1: Int = 0
+    var RandomDiceIndex2: Int = 0
+    
+    //IBoutlet: changes appearance of UI element
+    
+    //Common error: if you want to rename/delete something, you can't do it in the code file. If you rename it, you will have to delete the old code from XML source code. If you don't want to do that, right click on the image and click the cross to break the link.
+    
+    //right click the name-> refactor -> rename
+    
+    //UIBuilder variable named DiceImageView1 with type UIImageView
+    @IBOutlet weak var DiceImageView1: UIImageView!
+    
+    //UIBuild variable named DiceImageView2 with the type UIImageView
+    @IBOutlet weak var DiceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //Touch inside: user pressed inside the button and let go
+    //IBAction: notifies code when button is interacted with
+    @IBAction func RollButtonPressed(_ sender: UIButton) {
+        //creates new whole numbers between 0 and 5 for RandomDiceIndex1
+        RandomDiceIndex1 = Int.random(in: 0 ... 5)
+        
+        //creates new whole numbers between 0 and 5 for RandomDiceIndex2
+        RandomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        //testing the random number generator
+        print(RandomDiceIndex1)
+    }
+    
 }
 
