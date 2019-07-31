1341723546aefeb5d98261624ea484fb1d8ec08f
Justin <jelias153@g.rwu.edu>
2019-07-30 21:26:14 -0500

    Added Images and Initial Logic
    Added images for App + Dice faces
    Connected the storyboard to code
    Added logic for button press to generate random numbers.
    Added printline to test random number generator on roll press.

---
+++ DiceApp/Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Justin Elias on 7/30/19.
+//  Copyright © 2019 Justin Elias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
